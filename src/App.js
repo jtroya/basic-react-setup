d19c5b65ad37ee39823912d0b8046e0e21ba30c3
Jorge Troya <jtm@softtelecom.eu>
2018-04-24 10:15:49 +0200

    Agregada la funcionalidad de async

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import 'babel-polyfill';
 
 class App extends Component {
     render() {
         return (
             <div>
-                <p>Que pasa</p>
+                <p>Hola</p>
             </div>
         );
     }
